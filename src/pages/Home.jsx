6d974807f5d44ff4f3ade344c8680a773f4f83fa
Denisraj10 <denisraj2122@gmail.com>
2026-02-13 23:20:01 +0530

    home pagee

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,23 +10,35 @@ const Home = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+    <div className="p-6 flex flex-col items-center justify-center p-9 bg-gray-300">
 
       <div className="bg-white p-10 rounded-2xl shadow-lg text-center w-96">
         
-        <h1 className="text-3xl font-bold mb-4">
-          🎉 Welcome!
+        <h1 className="text-3xl text-red-300 font-bold mb-4">
+           Welcome!
         </h1>
+<div className="flex  mb-5 ">
+       <input
+        type='text'
+        holder='user name'
+        className='w-full  font-mono p-3 rounded-lg bg-gray-200 text-black border-2 border-b-gray-900 mb-4'
+       /><button
+          className=" text-white  "
+        >search </button>
+</div>
 
-        <p className="text-cyan-500 font-bold text-2xl mb-6">
-          
-        </p>
+<button className="flex " >bidding item</button>
+
+
+
 
         <button
           onClick={handleLogout}
           className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
         >
-          Click 
+          Click log out
+
+
         </button>
 
       </div>
